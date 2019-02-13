c8d40e9ee508eb67e05fabf2b7b611f5df4e3819
April Carter <aprilddev@gmail.com>
2019-02-13 00:05:38 -0500

    Email support and reset password setup

--- app/routes.py
+++ app/routes.py
@@ -18,9 +18,12 @@ from app.forms import (
     LoginForm,
     RegistrationForm,
     EditProfileForm,
-    PostForm
+    PostForm,
+    ResetPasswordRequestForm,
+    ResetPasswordForm
 )
 from app.models import User, Post
+from app.email import send_password_reset_email
 
 @app.before_request
 def before_request():
@@ -177,3 +180,33 @@ def explore():
                             posts=posts.items,
                             next_url=next_url,
                             prev_url=prev_url)
+
+
+@app.route('/reset_password_request', methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('If your email is found in the system, you will get a reset link soon.')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html.j2', title='Reset Password', form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_passwordd(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html.j2', form=form)
