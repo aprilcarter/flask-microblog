5bf70aad00635ba1da85923d89c8ee0280baee82
April Carter <aprilddev@gmail.com>
2019-02-17 19:24:05 -0500

    Debug email sending Flask-mail 2.0 doesn't have the send_email method. Have to use Message instance and send() method

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from threading import Thread
 from flask import render_template
-from app import mail, app
+from app import app, mail
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -15,10 +15,7 @@ def send_mail(subject, sender, recipients, text_body, html_body):
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Microblog] Reset Your Password',
-                sender=app.config['ADMINS'][0],
-                recipients=[user.email],
-                text_body=render_template('email/reset_password.txt',
-                                            user=user, token=token),
-                html_body=render_template('email/reset_password.html.j2',
-                                            user=user, token=token))
+    msg = Message('[Microblog] Reset Your Password', sender=app.config['ADMINS'][0], recipients=[user.email])
+    msg.body = render_template('email/reset_password.txt', user=user, token=token)
+    msg.html = render_template('email/reset_password.html.j2', user=user, token=token)
+    mail.send(msg)
